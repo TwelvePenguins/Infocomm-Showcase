d78b55fb608526a9fdce2f615e50141f90f2c3f3
Du Yuhan <yuhan.du@gmail.com>
2022-12-30 14:54:05 +0800

    Initial Commit

---
+++ IFC Showcase/IFC_ShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IFC_ShowcaseApp.swift
+//  IFC Showcase
+//
+//  Created by Yuhan Du on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct IFC_ShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
