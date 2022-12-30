d298d07321bc0a20ece797dd672e3cb8e620057a
Du Yuhan <yuhan.du@gmail.com>
2022-12-30 17:49:18 +0800

    Changed the method of navigation between different views

--- IFC Showcase/AchievementsView.swift
+++ IFC Showcase/AchievementsView.swift
@@ -8,8 +8,12 @@
 import SwiftUI
 
 struct AchievementsView: View {
+    
+    @State var showHomeView = false
+    @State var searchTerm = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("HALLO I HATE THIS")
     }
 }
 
--- IFC Showcase/HomeView.swift
+++ IFC Showcase/HomeView.swift
@@ -12,57 +12,57 @@ struct HomeView: View {
     @State var showAchievementsView = false
     
     var body: some View {
-        if showAchievementsView {
-            AchievementsView()
-        } else {
-            ZStack {
-                HStack(spacing: 0) {
-                    Rectangle()
-                        .foregroundColor(Color("BackgroundBlue"))
-                    Rectangle()
-                        .foregroundColor(Color("BackgroundGreen"))
-                }
-                VStack(alignment: .center) {
-                    Text("Infocomm\nClub")
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 100, weight: .bold, design: .serif))
-                    Text("Coding")
-                        .foregroundColor(.white)
-                        .font(.system(size: 50, design: .monospaced))
-                    HStack {
-                        Button {
-                            showAchievementsView = true
-                        } label: {
-                            ZStack(alignment: .center) {
-                                Circle()
-                                    .foregroundColor(Color("AccentBlue"))
-                                    .frame(width: 300)
-                                Text("Explore our achievements")
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth: 300)
-                                    .font(.system(size: 30, design: .monospaced))
-                                    .multilineTextAlignment(.center)
-                            }
-                            .padding(.leading, 100)
+        ZStack {
+            HStack(spacing: 0) {
+                Rectangle()
+                    .foregroundColor(Color("BackgroundBlue"))
+                Rectangle()
+                    .foregroundColor(Color("BackgroundGreen"))
+            }
+            VStack(alignment: .center) {
+                Text("Infocomm\nClub")
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 100, weight: .bold, design: .serif))
+                Text("Coding")
+                    .foregroundColor(.white)
+                    .font(.system(size: 50, design: .monospaced))
+                HStack {
+                    Button {
+                        showAchievementsView = true
+                    } label: {
+                        ZStack(alignment: .center) {
+                            Circle()
+                                .foregroundColor(Color("AccentBlue"))
+                                .frame(width: 300)
+                            Text("Explore our achievements")
+                                .foregroundColor(.white)
+                                .frame(maxWidth: 300)
+                                .font(.system(size: 30, design: .monospaced))
+                                .multilineTextAlignment(.center)
                         }
-                        Spacer()
-                        Link(destination: URL(string: "https://form.gov.sg/63ae667af03e1f00121e1e44")!) {
-                            ZStack(alignment: .center) {
-                                Circle()
-                                    .foregroundColor(Color("AccentGreen"))
-                                    .frame(width: 300)
-                                Text("Sign up")
-                                    .frame(maxWidth: 300)
-                                    .foregroundColor(.white)
-                                    .font(.system(size: 50, design: .monospaced))
-                            }
-                            .padding(.trailing, 100)
+                        .padding(.leading, 100)
+                        .fullScreenCover(isPresented: $showAchievementsView) {
+                            AchievementsView()
                         }
+                    }
+                    Spacer()
+                    Link(destination: URL(string: "https://form.gov.sg/63ae667af03e1f00121e1e44")!) {
+                        ZStack(alignment: .center) {
+                            Circle()
+                                .foregroundColor(Color("AccentGreen"))
+                                .frame(width: 300)
+                            Text("Sign up")
+                                .frame(maxWidth: 300)
+                                .foregroundColor(.white)
+                                .font(.system(size: 50, design: .monospaced))
+                        }
+                        .padding(.trailing, 100)
                     }
                 }
             }
-            .ignoresSafeArea()
         }
+        .ignoresSafeArea()
     }
 }
+
