0a541095ddcec2a1a0d0d7b84c3d2758e4f8eb6d
Du Yuhan <yuhan.du@gmail.com>
2022-12-31 21:00:51 +0800

    Added formatting of achievements, altered assets to be landscape

--- IFC Showcase/AchievementsView.swift
+++ IFC Showcase/AchievementsView.swift
@@ -11,19 +11,67 @@ struct AchievementsView: View {
     
     var achievementsList: [Achievement] = [Achievement(image: "Digital For Life Youth Awards 2022, Media Accomplishment", year: 2022, title: "Digital For Life Youth Awards 2022, Media Accomplishment"), Achievement(image: "Digital For Life Youth Awards 2022, Tech Accomplishment", year: 2022, title: "Digital For Life Youth Awards 2022, Tech Accomplishment"), Achievement( image: "Imagine Cup Junior Singapore 2022", year: 2022, title: "Imagine Cup Junior Singapore 2022, Top 10 Team"), Achievement(image: "Swift Explorers 2022", year: 2022, title: "Swift Explorers 2022, Finalist"), Achievement(image: "SIS 2022, Remorise", year: 2022, title: "App presentation of Remorise at Swift Innovators' Summit 2022")]
     
+    //yes this is stupid but i dont want to make an achievement manager thank you :)
+    
     @State var searchTerm = ""
     
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        List {
-            ForEach(achievementsList) { achievement in
-                
+        ZStack {
+            Color("BackgroundBlue")
+            NavigationStack {
+                ScrollView {
+                    VStack(alignment: .center) {
+                        ForEach(achievementsList) { achievement in
+                            VStack(alignment: .leading, spacing: -7) {
+                                ZStack(alignment: .center) {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .foregroundColor(Color(getBackgroundColor(for: achievement.year)))
+                                        .frame(width: 120, height: 40)
+                                    Text(String(achievement.year))
+                                        .font(.system(size: 20, design: .monospaced))
+                                        .foregroundColor(.white)
+                                }
+                                ZStack(alignment: .center){
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .foregroundColor(Color(getBackgroundColor(for: achievement.year)))
+                                        .frame(maxWidth: 600)
+                                        .controlSize(.large)
+                                    VStack(alignment: .center){
+                                        Image(achievement.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 300)
+                                            .mask(RoundedRectangle(cornerRadius: 5))
+                                        Text(achievement.title)
+                                            .foregroundColor(.white)
+                                            .frame(maxWidth: 500)
+                                            .font(.system(size: 20, weight: .bold, design: .serif))
+                                            .multilineTextAlignment(.center)
+                                            .padding(.top, 10)
+                                    }
+                                    .padding(20)
+                                }
+                            }
+                        }
+                        .padding(30)
+                    }
+                    .padding(.leading, 21)
+                    .padding(.trailing, 21)
+                    .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for an achievement of the past 3 years")
+                }
             }
-        }
-        .toolbar {
-            Text("Test")
         }
     }
 }
 
+func getBackgroundColor(for year: Int) -> String {
+    if year == 2022 {
+        return "Blue2022"
+    } else if year == 2021 {
+        return "Blue 2021"
+    } else {
+        return "Blue 2020"
+    }
+}
